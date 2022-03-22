0965dca8bc0181610a63411228ed602ae1420950
Chad Cormier Roussel <chadcormierroussel@gmail.com>
2022-03-22 20:11:43 +0000

    Fix struct alignment.
    This was always an error, but wouldn't stop compilation until gcc 11.
    
    see: https://bugzilla.redhat.com/show_bug.cgi?id=1937076 for reference.

--- algorithms/sha3/sph_blake2s.c
+++ algorithms/sha3/sph_blake2s.c
@@ -327,7 +327,7 @@ int blake2s_final( blake2s_state *S, uint8_t *out, uint8_t outlen )
 
 int blake2s( uint8_t *out, const void *in, const void *key, const uint8_t outlen, const uint64_t inlen, uint8_t keylen )
 {
-	blake2s_state S[1];
+	blake2s_state S[64];
 
 	/* Verify parameters */
 	if ( NULL == in ) return -1;
